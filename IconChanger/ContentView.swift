48bbed438005016c4d5c5bae569265cd9668b7c3
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2022-04-27 16:22:33 +0800

    Initial Commit

---
+++ IconChanger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IconChanger
+//
+//  Created by 朱浩宇 on 2022/4/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IconChanger/IconChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconChangerApp.swift
+//  IconChanger
+//
+//  Created by 朱浩宇 on 2022/4/27.
+//
+
+import SwiftUI
+
+@main
+struct IconChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
